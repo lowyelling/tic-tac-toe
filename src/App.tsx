1a538084c7c73eaadf5d31b39feaba86cffb1c8e
Lilyfc <coralcatcodes@gmail.com>
2026-02-03 15:56:59 -0500

    setState upon clicking each <td>

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ function App() {
     <>
       <table>
         <tr>
-          <td>{gameState.board[0] || "_"}</td><td>{gameState.board[1] || "_"}</td><td>{gameState.board[2] || "_"}</td>
+          <td onClick={() => setGameState(makeMove(gameState, 0))}>{gameState.board[0] || "_"}</td><td onClick={() => setGameState(makeMove(gameState, 1))}>{gameState.board[1] || "_"}</td><td onClick={() => setGameState(makeMove(gameState, 2))}>{gameState.board[2] || "_"}</td>
         </tr>
         <tr>
-          <td>{gameState.board[3] || "_"}</td><td>{gameState.board[4] || "_"}</td><td>{gameState.board[5] || "_"}</td> 
+          <td onClick={() => setGameState(makeMove(gameState, 3))}>{gameState.board[3] || "_"}</td><td onClick={() => setGameState(makeMove(gameState, 4))}>{gameState.board[4] || "_"}</td><td onClick={() => setGameState(makeMove(gameState, 5))}>{gameState.board[5] || "_"}</td> 
         </tr>
          <tr>
-         <td>{gameState.board[6] || "_"}</td><td>{gameState.board[7] || "_"}</td><td>{gameState.board[8] || "_"}</td>
+         <td onClick={() => setGameState(makeMove(gameState, 6))}>{gameState.board[6] || "_"}</td><td onClick={() => setGameState(makeMove(gameState, 7))}>{gameState.board[7] || "_"}</td><td onClick={() => setGameState(makeMove(gameState, 8))}>{gameState.board[8] || "_"}</td>
          </tr>
       </table>
       <div>Hello World! current player: {gameState.currentPlayer}</div>
@@ -26,8 +26,8 @@ function App() {
 
 function getInitialGame() {
   let initialGameState = createGame()
-  initialGameState = makeMove(initialGameState, 3)
-  initialGameState = makeMove(initialGameState, 0)
+  // initialGameState = makeMove(initialGameState, 3)
+  // initialGameState = makeMove(initialGameState, 0)
   return initialGameState
 }
 
