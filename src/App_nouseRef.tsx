c2283b4754ba65880c09666bf32a1ef280aa49db
Lilyfc <coralcatcodes@gmail.com>
2026-02-07 14:07:24 -0500

    remove useRef from App_nouseRef to avoid TS error

--- src/App_nouseRef.tsx
+++ src/App_nouseRef.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { type GameState, getWinner } from "./tic-tac-toe";
 import "./App.css"; 
 
